de669f884f303fc41b658cbb71a8de6762e77dd4
Max Ogden <mogden@gmail.com>
2012-03-22 17:15:29 -0700

    add screenshot

--- couchapp.js
+++ couchapp.js
@@ -3,15 +3,16 @@ var couchapp = require('couchapp')
   ;
 
 ddoc = 
-  { _id:'_design/catmapper'
-  , rewrites: 
+  { "_id":'_design/catmapper'
+  , "description": "jQuery mobile app that collects specific locations from users"
+  , "name": "Mobile Mapper"
+  , "rewrites": 
     [ {from:"/", to:'index.html'}
-    , {from:"/api", to:'../../'}    
-    , {from:"/api/*", to:'../../*'}
+    , {from:"/api", to:'/../../'}
+    , {from:"/api/*", to:'/../../*'}
     , {from:"/*", to:'*'}
     ]
-  }
-  ;
+  };
 
 couchapp.loadAttachments(ddoc, path.join(__dirname, 'attachments'));
 
